d90cedb0606a22ba8f9750da0026639fbf99dfec
okolo kamdilichukwu samuel <150658859+boi-network12@users.noreply.github.com>
2026-01-15 21:20:39 +0100

    .

--- client/config/api.ts
+++ client/config/api.ts
@@ -1,6 +1,7 @@
 export const BACKEND_URI = 'https://neuromed-ai-backend.vercel.app';
 // For production: 'https://neuromed-ai-backend.vercel.app'
-// Local host API: http://10.243.157.4:8080
+// Local host API: http://10.151.164.4:8080
 // 
 export const SOCKET_URL = "https://inner-gay-kamdidev-58357c79.koyeb.app";
 // https://inner-gay-kamdidev-58357c79.koyeb.app
+// http://10.151.164.4:30001
--- socket-server/src/config/baseApi.ts
+++ socket-server/src/config/baseApi.ts
@@ -1,4 +1,4 @@
 // socket-server/src/config/baseApi
 export const BASE_URL = "https://neuromed-ai-backend.vercel.app"
 // https://neuromed-ai-backend.vercel.app
-// socket-server/config/baseAPI.ts
+// http://10.151.164.4:8080
